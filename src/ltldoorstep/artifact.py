11dac23f5c257d802cdd700f6443d80458abd6be
Phil Weir <phil.weir@flaxandteal.co.uk>
2020-05-12 20:40:28 +0100

    improve handling and recording of artifacts

--- src/ltldoorstep/artifact.py
+++ src/ltldoorstep/artifact.py
@@ -3,13 +3,13 @@ from dataclasses import dataclass
 @dataclass
 class ArtifactType:
     '''Class for tracking a (potential) artifact's nature'''
-    mime: str = None
     is_bytes: bool
+    mime: str = None
 
 
 @dataclass
 class ArtifactRecord:
     '''Class for tracking an actual artifact'''
     uri: str
+    typ: ArtifactType
     mime: str = None
-    is_bytes: bool
